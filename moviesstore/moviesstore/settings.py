8baddf885121c7d5f1f1dae0507bf65425e5c253
StudioMitesh <miteshmanishshah@gmail.com>
2025-02-02 23:06:29 -0500

    finished ch4,5,10, added cart and movies sets

--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "home",
     'accounts',
+    "cart",
+    "movies",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     BASE_DIR / 'moviesstore/static/',
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- moviesstore/moviesstore/urls.py
+++ moviesstore/moviesstore/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('home.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
+    path('movies/', include('movies.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, root_patterns=settings.MEDIA_ROOT)
+
