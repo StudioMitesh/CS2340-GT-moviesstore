657911c225c38f822032005e846fff5ad45915cd
Steven Zhou <stevenzdragons@gmail.com>
2025-02-03 02:16:21 -0500

    Merged with main (overrided some parts)

--- moviesstore/home/urls.py
+++ moviesstore/home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='home.index'),
-    path('about', views.about, name='home.about'),
+    path('about', views.about, name = "home.about"),
 ]
